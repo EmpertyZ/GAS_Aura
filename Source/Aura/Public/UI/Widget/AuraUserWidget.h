2c29eed1105434dd6d876420d292463a30063c3a
unknown <zkh15766349066@gmail.com>
2024-03-25 22:26:24 +0800

    更新了基础的血量ui

---
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	void SetWidgetController(UObject* InWidgetController);
+	
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)//BlueprintImplementableEvent:在C++可以声明函数（不能定义，蓝图重写），在C++里调用该函数，蓝图重写实现该函数
+	void WidgetControllerSet();
+};
